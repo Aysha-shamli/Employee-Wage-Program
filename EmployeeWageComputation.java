7b43acf67626179ec4c8ca056e3c126036b3249d
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-04 12:21:49 +0530

    added program to check employee is present or not

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,8 +1,21 @@
+import java.util.Random;
+
 public class EmployeeWageComputation {
     public static void display(){
         System.out.println("Welcome to Employee Wage Computation Program");
     }
+    public static void employeeCheck(){
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+    }
     public static void main(String[] args) {
         display();
+        employeeCheck();
     }
 }
