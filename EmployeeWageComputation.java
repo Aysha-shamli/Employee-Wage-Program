c044e5fba1018f27d8027f1c57d12d247f769364
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-04 12:26:25 +0530

    added program to find daily wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,18 +4,26 @@ public class EmployeeWageComputation {
     public static void display(){
         System.out.println("Welcome to Employee Wage Computation Program");
     }
-    public static void employeeCheck(){
+
+    public static void employeeCheck(int wagePerHour, int hoursWorked){
         Random random = new Random();
         int attendance = random.nextInt(2);
         if (attendance == 1) {
             System.out.println("Employee is Present");
+
+            int dailyWage = wagePerHour * hoursWorked;
+            System.out.println("Employee's Daily Wage: " + dailyWage);
         } else {
             System.out.println("Employee is Absent");
         }
-
     }
+
     public static void main(String[] args) {
         display();
-        employeeCheck();
+
+        int wagePerHour = 20;
+        int hoursWorked = 8;
+
+        employeeCheck(wagePerHour, hoursWorked);
     }
 }
