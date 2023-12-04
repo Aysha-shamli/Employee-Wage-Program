796488d9184a0b1d1a44bd91330fad47ef796569
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-04 12:11:10 +0530

    added welcome message displaying program to master branch

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+public class EmployeeWageComputation {
+    public static void display(){
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+    public static void main(String[] args) {
+        display();
+    }
+}
