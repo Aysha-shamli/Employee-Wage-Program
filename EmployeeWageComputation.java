60adf7038fc013f5d1cb0b5dbbe783f6d5941353
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-04 12:46:40 +0530

    added program to find monthly wage of employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,6 +7,7 @@ public class EmployeeWageComputation {
     }
 
     public static void employeeCheck(int wagePerHour, int fullTimeHours, int partTimeHours) {
+        int workingDaysPerMonth = 20;
         Random random = new Random();
         int attendance = random.nextInt(2);
         if (attendance == 1) {
@@ -16,16 +17,20 @@ public class EmployeeWageComputation {
             System.out.println("Enter 1 for Full-Time Employee ");
             System.out.print("Enter 2 for Part-Time Employee ");
             int input = scanner.nextInt();
-            int dailyWage;
+
 
             switch (input) {
                 case 1:
-                    dailyWage = wagePerHour * fullTimeHours;
+                    int dailyWage = wagePerHour * fullTimeHours;
+                    int fullTimeWage = wagePerHour * fullTimeHours * workingDaysPerMonth;
                     System.out.println("Full-Time Employee's Daily Wage: " + dailyWage);
+                    System.out.println("Full-Time Employee's Monthly Wage: " + fullTimeWage);
                     break;
                 case 2:
                     dailyWage = wagePerHour * partTimeHours;
+                    int partTimeWage = wagePerHour * partTimeHours * workingDaysPerMonth;
                     System.out.println("Part-Time Employee's Daily Wage: " + dailyWage);
+                    System.out.println("Part-Time Employee's Monthly Wage: " + partTimeWage);
                     break;
                 default:
                     System.out.println("Invalid Employee Type");
